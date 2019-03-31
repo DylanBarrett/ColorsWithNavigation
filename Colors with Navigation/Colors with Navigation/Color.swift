de0173bbe266195563fd9549481d8e578258b37b
Dylan Barrett <42352484+DylanBarrett@users.noreply.github.com>
2019-03-31 13:50:42 -0500

    Add files via upload

---
+++ Colors with Navigation/Colors with Navigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors with Navigation
+//
+//  Created by Dylan barrett on 3/31/19.
+//  Copyright © 2019 Dylan barrett. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
---
+++ Colors with Navigation/Colors with Navigation/ColorDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ColorDetailViewController.swift
+//  Colors with Navigation
+//
+//  Created by Dylan barrett on 3/31/19.
+//  Copyright © 2019 Dylan barrett. All rights reserved.
+//
+
+import UIKit
+
+class ColorDetailViewController: UIViewController {
+    
+    @IBOutlet weak var colorNameLabel: UILabel!
+    
+    var color: Color?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    colorNameLabel.text = color?.name
+        
+    self.view.backgroundColor = color?.uiColor
+        
+    self.title = color?.name
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
